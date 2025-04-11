8025ea827ac0955cd4646193ea6d84ceadc2f4b9
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 16:59:35 +0400

    Add files via upload

---
+++ JS 6/JS 7/app.js
@@ -0,0 +1,54 @@
+// დავალება 1
+
+// function hello() {
+//     console.log("Hello JS")
+
+// }
+
+// hello()
+
+
+
+
+
+// დავალება 2
+
+// function randomNumber() {
+
+//     return Math.round(Math.random() * 100)
+ 
+// }
+
+// console.log(randomNumber())
+
+
+
+
+
+// დავალება 3
+
+// let username = prompt("Enter Your Name")
+
+// function hello() {
+//     document.write(`გამარჯობა, ${username}`)
+// }
+
+// hello()
+
+
+
+
+
+
+
+
+
+
+// დავალება 4
+
+// alert("web page will save your data")   
+// function welcome() {
+//     document.write("welcome")
+    
+// }
+// welcome()
---
+++ JS 6/app.js
@@ -0,0 +1,63 @@
+// 1 დავალება
+// let names = ["გიორგი", "ანა", "მარიამი", "დავით", "თორნიკე"];
+// let surnames = ["გოგოხია", "ნადირაძე", "ჯიშკარიანი", "კობიაშვილი", "შაქარიანი"];
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(`${names[i]} ${surnames[i]}`);
+// }
+
+
+
+
+// 2 დავალება 
+
+// let arr2D = [
+//     [1, 12, 3],
+//     [14, 5, 6],
+//     [7, 8, 15]
+// ]
+
+// for(let i = 0; i < arr2D.length; i++){
+//     for(let j = 0; j < arr2D[i].length; j++){
+//         if(arr2D[i][j] > 10){
+//             console.log(arr2D[i][j])
+//         }
+//     }
+// }
+
+
+
+
+
+
+// 3 დავალება
+// let arr = [[20, 60, 30], [15, 10, 40]];
+
+// for (let i = 0; i < arr.length; i++) {
+//     let firstElement = arr[i][0];
+//     let lastElement = arr[i][arr[i].length - 1];
+//     let sum = firstElement + lastElement;
+//     console.log(sum);
+// }
+
+
+
+
+// 4 დავალება 
+// let arr = [
+//     [10, 20, 30],
+//     [5, 15, 25],
+//     [1, 2, 3],
+//     [100, 200, 300],
+//     [50, 60, 70]
+// ];
+
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         sum += arr[i][j];
+//     }
+// }
+
+// console.log("ჯამი :", sum);
