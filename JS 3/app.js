4b9912f46f9b9b4b54b8d92fefafd69cc079cd37
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 16:58:53 +0400

    Add files via upload

---
+++ JS 3/app.js
@@ -0,0 +1,106 @@
+// დავალება 1
+
+// let min = 10;
+// let max = 20;
+
+// const randomNumber = Math.round(Math.random() * (max - min) + min);
+// console.log("Random number:", randomNumber);
+
+// let UserNumber = Number(prompt("Enter The Number Between 10-20"));
+
+// if(UserNumber === randomNumber) {
+//     console.log("True");
+// } 
+
+// else if(UserNumber >= min && UserNumber <= max) {
+//     console.log("False");
+// } 
+
+// else{
+//     console.log("Wrong input! Please enter a number between 10 and 20.");
+// }
+
+
+
+
+
+
+
+
+// დავალება 2
+
+// let min = 100;
+// let max = 300;
+
+// const randomNumber = Math.round(Math.random() * (max - min) + min);
+// console.log("Random number:", randomNumber);
+
+
+// if (randomNumber % 2 === 0) {
+//     console.log("The number is even - ლუწი");
+// } 
+// else if(randomNumber % 2 !== 0) {
+//     console.log("The number is odd - კენტი");
+// } 
+
+
+
+
+
+
+
+
+
+// დავალება 3
+
+// let min = 50;
+// let max = 150;
+
+// const randomNumber = Math.round(Math.random() * (max - min) + min)
+
+
+// let userInput = Number(prompt("Enter a number between 50 and 150:"));
+
+// if (randomNumber % 3 && randomNumber % 5) {
+//     console.log("ეს არის ჯერადი");
+// } 
+// else {
+//     console.log("არ არის");
+// }
+
+
+
+
+
+
+
+
+
+
+// დავალება 4
+
+// let min = 10
+// let max = 100
+
+// const randomNumber = Math.round(Math.random() * (max - min) + min);
+// console.log("Random number:", randomNumber);
+
+// let userInput = Number(prompt("Enter a number between 10 and 100"))
+
+// if(userInput > randomNumber){
+//     console.log("მეტია")
+// }
+
+// else if(userInput < randomNumber){
+//     console.log("ნაკლებია")
+// }
+
+// else if(userInput == randomNumber){
+//     console.log("ტოლია")
+// }
+
+// else{
+//     console.log("Wrong Input")
+// }
+
+
