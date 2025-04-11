c076e0d1cc9db9a8b80afc248d5d58cb38e3093e
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 16:58:40 +0400

    Add files via upload

---
+++ JS 2/app.js
@@ -0,0 +1,43 @@
+console.log("Hello Lana <3")
+
+let x = prompt("Enter The Number")
+
+if(x == "20+10"){
+    console.log(30)
+}
+
+else{
+    console.log("Error")
+}
+
+
+
+let y = prompt("Enter The Password")
+
+if(y == "abc123"){
+    console.log("Correct")
+}
+
+else{
+    console.log("Error")
+}
+
+
+
+let z = prompt("Add 3 Sides")
+
+if(z == "5+2+7"){
+    console.log("You Can Create A Triangle")
+}
+
+else{
+    console.log("You Can't Create A Triangle")
+}
+
+
+
+
+
+
+
+
