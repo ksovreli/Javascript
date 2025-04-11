3f76b778cc647725b70f1483d6c6688c8ab8b5ab
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 16:59:15 +0400

    Add files via upload

---
+++ JS 5/app.js
@@ -0,0 +1,95 @@
+// დავალება1
+
+// let numArr = [10,43,64,78,91]
+// let countEven =0
+// let countOdd =0
+// for(let i =0; i<numArr.length; i++) {
+//    if(numArr[i] %2 ==0 ){
+//     countEven++
+//     console.log(numArr[i], "Even")
+//    }
+//    else {
+//     countOdd++
+//     console.log(numArr[i], "Odd")
+//    }
+// }
+// console.log(countEven, countOdd)
+
+// დავალება 2
+
+// let arrayNum = [20,61,42,71,80, 91]
+
+// for(let i = 0; i<=arrayNum.length; i++){
+//     if(i%2==0 && arrayNum[i]%2==0){
+//         console.log(arrayNum[i], "Even")
+//     }
+//     else{
+//         console.log(arrayNum[i], "Odd")
+//     }
+// }
+
+
+
+// დავალება 3
+
+
+// for(let i=-100; i<=100; i++){
+//     if(i%2==0 && i%3==0){
+//         console.log(i)
+//     }
+
+//     else{
+//         console.log(false)
+//     }
+// }
+
+// დავალება 4
+
+// for(let i=-100; i<=100; i++){
+//     if(i%4==1 || i%4==2){
+//         console.log(i)
+//     }
+
+//     else{
+//         console.log("wrong")
+//     }
+// }
+
+
+// დავალება 5
+
+
+// let numArray = ["Orange", "Grape", "Strawberry", "Watermelon", "Melon"];
+
+// for (let i = 0; i < numArray.length; i++){
+//     console.log(numArray[i]);
+
+// for (let j = 0; j < numArray[i].length; j++){
+//     console.log(numArray[i][j]);
+// }
+// }
+
+
+// დავალება 6 
+
+// let numArray = [71,98,90,102]
+
+// for(let i = 0; i<numArray.length; i++){
+//     console.log(i)
+
+//     if(numArray[0]<numArray[1]){
+//         console.log(numArray[0], "Nაკლებია")
+//     }
+
+//     else if(numArray[1]>numArray[2]){
+//         console.log(numArray[1], "მეტია")
+//     }
+
+//     else if(numArray[2]<numArray[3]){
+//         console.log(numArray[2], "ნაკლებია")
+//     }
+    
+//     else{
+//         console.log("Wrong")
+//     }
+// }
