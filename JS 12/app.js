2bffac9b68769e29ae5edafca2af7270cb78dcd5
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 17:01:41 +0400

    Add files via upload

---
+++ JS 12/app.js
@@ -0,0 +1,59 @@
+let cont = document.querySelector(".cont")
+let jsonObc = `[
+    {
+      "id": 1,
+      "email": "george.bluth@reqres.in",
+      "first_name": "George",
+      "last_name": "Bluth",
+      "avatar": "https://reqres.in/img/faces/1-image.jpg"
+    },
+    {
+      "id": 2,
+      "email": "janet.weaver@reqres.in",
+      "first_name": "Janet",
+      "last_name": "Weaver",
+      "avatar": "https://reqres.in/img/faces/2-image.jpg"
+    },
+    {
+      "id": 3,
+      "email": "emma.wong@reqres.in",
+      "first_name": "Emma",
+      "last_name": "Wong",
+      "avatar": "https://reqres.in/img/faces/3-image.jpg"
+    },
+    {
+      "id": 4,
+      "email": "eve.holt@reqres.in",
+      "first_name": "Eve",
+      "last_name": "Holt",
+      "avatar": "https://reqres.in/img/faces/4-image.jpg"
+    },
+    {
+      "id": 5,
+      "email": "charles.morris@reqres.in",
+      "first_name": "Charles",
+      "last_name": "Morris",
+      "avatar": "https://reqres.in/img/faces/5-image.jpg"
+    },
+    {
+      "id": 6,
+      "email": "tracey.ramos@reqres.in",
+      "first_name": "Tracey",
+      "last_name": "Ramos",
+      "avatar": "https://reqres.in/img/faces/6-image.jpg"
+    }
+]`
+
+
+let objJs = JSON.parse(jsonObc)
+for(let el of objJs){
+    cont.innerHTML += `
+    <div class="card" style="width: 18rem;">
+  <img src="${el.avatar}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${el.first_name} ${el.last_name}</h5>
+    <p class="card-text">${el.email}</p>
+    <a href="#" class="btn btn-primary">Message Me 🤫</a>
+  </div>
+</div>`
+}
