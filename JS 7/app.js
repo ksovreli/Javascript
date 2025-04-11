a096e6081b2b6dda5f7b88b9fec0b21be8103c9d
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 17:00:27 +0400

    Add files via upload

---
+++ JS 7/app.js
@@ -0,0 +1,96 @@
+//დავალება 1
+
+// function filterStrings(array, str) {
+//      return array.filter(el => el.includes(str));
+// }
+
+// let stringsArray = ["apple", "banana", "cherry", "apricot"];
+// let searchString = "ap";
+// let filteredStrings = filterStrings(stringsArray, searchString);
+
+// console.log(filteredStrings);
+
+
+
+// დავალება 2
+
+// let arr = [1,2,3,4,5,6,7,8  ,9,10,11,12 ,   12, ]
+// let arr2 = [10,20]
+// function filter(array, array2) {
+//      for(let i = 0; i < arr.length; i++) {
+
+//         for(let j = 0; j <arr2.length; j++) {
+
+//             if(arr[i] === arr2[j]) {
+//                 return array.filter(el => el => arr)
+//             }
+        
+//         }
+//     }
+
+    
+//  }
+
+
+// console.log(filter( arr2));
+
+
+
+//დავალება 3
+
+// function wordCheck(word, array) {
+//     for (let sentence of array) {
+//         if (sentence.includes(word)) {
+//             return true
+//         }
+
+//         else{
+//             return false;
+//         }
+//     }
+    
+    
+// }
+
+// let word = "მეგობარი";
+// let array = [
+//     "ჩემი მეგობარი კარგი ადამიანია",
+//     "ჩემი ძმა კარგი ადამიანია"
+
+// ]
+
+// console.log(wordCheck(word, array));
+
+
+
+
+
+
+
+// დავალება 4
+// function IsHigherThan(number, array) {
+//     for (let num of array) {
+//         if (num > number) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// let number = 10;
+// let numbersArray = [3, 5, 8, 12, 7];
+
+// console.log(IsHigherThan(number, numbersArray));
+
+
+
+// დავალება 5
+
+// function multipy(array) {
+//     return array.map(num => num * 10)
+// }
+
+// let numbersArray = [1, 2, 3, 4, 5]
+
+// console.log(multipy(numbersArray))
+
