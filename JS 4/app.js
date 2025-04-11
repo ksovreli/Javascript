4c1861e2c5d98791e64d719ce9d38a71ca1406a3
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 16:59:03 +0400

    Add files via upload

---
+++ JS 4/app.js
@@ -0,0 +1,64 @@
+// დავალება 1
+
+// for(let i =100; i!=200; i+=2){
+
+
+//        if(i%2 == 0){
+//         console.log(i)
+//        }
+//     }
+    
+
+
+//     for(let i=300; i!=400; i+=2){
+//         if(i%2 == 0){
+//             console.log(i)
+//         }
+//     }
+
+
+    // დავალება 2
+
+
+// let userInput = Number(prompt("Enter Number Between 50 to 100"))
+// for(let i =50; i!=userInput; i++){
+//     console.log(i)
+// }
+
+
+// დავალება 3
+
+// for(let i=100; i!=200; i++){
+//     console.log(i)
+// }
+
+
+// let userInput = Number(prompt("Enter Number Between 50 to 100"))
+
+// for(let i=300; i<userInput; i++){
+//     console.log(i)
+
+
+//     if (randomNumber % 3 == 0) {
+//         console.log("ეს არის ჯერადი");
+//     } 
+//     else {
+//         console.log("არ არის");
+//     }
+// }
+
+
+//
+
+
+// დავალება 4
+
+// const test = "HaHa"
+
+// for(let i=0; i<test.length; i++){
+//     console.log(test[i])
+// }
+
+
+
+
