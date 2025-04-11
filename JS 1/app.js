a2ab81714c893bc9cb6c162295c16af6a749008f
ksovreli <sandroqsovreli@gmail.com>
2025-04-11 16:58:24 +0400

    Add files via upload

---
+++ JS 1/app.js
@@ -0,0 +1,78 @@
+
+console.log("g")
+console.log(-2.5)
+console.log(true)
+console.log(false)
+console.log(null)
+    //  მონაცემთა ტიპები
+         //  სტრინგი ტექსტი
+         //  ნამბერი რიცხვი 
+         //  ბულიანი TRUE FALSE   BOOLLEAN
+         //  null
+         //  undefined
+         //  object     
+
+    // ცვლადი variable და მისი დეკლარირება გამოცხადება ინიციალიზება  
+    
+    // x=5
+    // y=10
+
+    // x+y = 15
+    // username = "Giorgi"
+    // username = "Sandro"
+
+         
+    // var   ცვლილებაც შეიძლება და მისი რედეკლარირებაც
+    // var x = 5
+    // var x = 10
+
+    // x = 25
+
+
+
+    // let   ცვლილება შეიძლება მაგრამ რედეკლარირება არ შეიძლება 
+
+    // let userName = "John"
+    // userName = "Jane"
+    // userName = "Someone"
+
+
+    // let userName // ერორი
+
+
+
+
+
+
+    // const      კონსტანტა მუდმივი და მისი არც ვალიუს შეცვლა შეიძლება და არც ახლიდან დეკლარირება,  რედეკლარირება
+
+    // const pi = 3.15
+    // pi = 3.145   // ერორია
+    // const pi = 20
+
+
+
+
+
+
+    //////////////  /////  ""   ''  ``
+
+    let age = 25
+    let userName = `JohnDoe`
+    
+    console.log(age)
+
+    console.log(userName + " არის " + age + " წლის")
+
+      ////  სტრინგის ინტერპოლაცია  სტრინგის გამოსახვა
+
+
+   /// გამომტანი ფუნქციები
+
+    console.log(`${userName} არის ${age} წლის`)   
+    
+    document.write(`${userName} არის ${age} წლის`)
+
+    alert("SoS!!!")
+
+    
